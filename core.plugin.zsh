76f8edc0819ae0106bd20c87b133e7a8383ba6e9
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-08-27 17:03:29 +0430

    .

--- core.plugin.zsh
+++ core.plugin.zsh
@@ -1,10 +1,4 @@
-echo 1 $0
-echo 2 ${0:#$ZSH_ARGZERO}
-echo 3 ${(%):-%N}
-echo 4 $ZERO
-echo 5 $PWD
-
-MYDIR="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+MYDIR="${${0:#$ZSH_ARGZERO}:-${(%):-%N}}"
 MYDIR="${${(M)MYDIR:#/*}:-$PWD/$MYDIR}"
 function night-source() {
     local dir="${2:-$MYDIR/load}"
